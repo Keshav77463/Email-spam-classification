8049b23f1fa0715a5973186e114c8b33e272a571
Keshav77463 <keshavsachdevarajp@gmail.com>
2026-02-02 22:37:13 +0530

    Add TF-IDF vectorization

--- spamclassifier.py
+++ spamclassifier.py
@@ -2,6 +2,7 @@ import pandas as pd
 from imblearn.over_sampling import SMOTE
 import re
 from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 df = pd.read_csv("data/spam.csv", encoding="latin-1")
 print(df.head())
@@ -27,3 +28,13 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42, stratify=y
 )
 print(X_train.head())
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+vectorizer = TfidfVectorizer(stop_words="english", max_features=3000)
+
+X_train_vec = vectorizer.fit_transform(X_train)
+X_test_vec  = vectorizer.transform(X_test)
+
+print(X_train_vec.shape)
+print(X_test_vec.shape)
+
